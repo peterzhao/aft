6883b2541b12f6820350ab6e286843d199d3b954
pzhao <peter.qs.zhao@gmail.com>
2012-07-26 22:55:01 -0400

    fix aft sql time out issue

--- SalsaImporterTests/PerformanceTest/SyncTests.cs
+++ SalsaImporterTests/PerformanceTest/SyncTests.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.Linq;
 using NUnit.Framework;
 using SalsaImporter;
@@ -12,45 +10,41 @@ namespace SalsaImporterTests.PerformanceTest
     [Category("PerformanceTest")]
     public class SyncTests
     {
-        private static void SaveSupportersToSalsa(SalsaClient salsa, int start, int blockSize)
+        private static void SaveSupportersToSalsa(AftDbContext db, SalsaClient salsa, int start, int blockSize)
         {
-            var db = new AftDbContext();
             var mapper = new SupporterMapper();
-            List<NameValueCollection> supporters =
-                db.Supporters.OrderBy(s => s.Id).Skip(start).Select(mapper.ToNameValues).Take(blockSize).ToList();
+            var supporters = db.Supporters.OrderBy(s => s.Id).Skip(start).Take(blockSize).ToList()
+                .Select(mapper.ToNameValues).ToList();
             salsa.SaveSupporters(supporters);
             supporters.ForEach(s =>
                                    {
-                                       int uid = int.Parse(s["uid"]);
-                                       Console.WriteLine("uid:" + uid);
-                                       Supporter supporter = db.Supporters.Find(uid);
-                                       string supporterKey = s["supporter_KEY"];
-                                       Console.WriteLine("SupportKey:" + supporterKey);
-                                       supporter.supporter_KEY = int.Parse(supporterKey);
+                                       var supporter = db.Supporters.Find(int.Parse(s["uid"]));
+                                       supporter.supporter_KEY = int.Parse(s["supporter_KEY"]);
                                    });
-
+            Logger.Debug("update aft db for supporter_key.");
             db.SaveChanges();
         }
+
 
         [Test]
         public void ShouldPushSupportersToSalsa()
         {
-            DateTime begin = DateTime.Now;
+            var begin = DateTime.Now;
             var salsa = new SalsaClient();
             salsa.Authenticate();
             salsa.DeleteAllSupporters();
             var db = new AftDbContext();
-            int total = db.Supporters.Count();
+
+            var total = db.Supporters.Count();
             total = 1000;
-            int blockSize = 100;
-            for (int i = 0; i < total; i += blockSize)
+            var blockSize = 100;
+            for (var i = 0; i < total; i += blockSize)
             {
                 Console.WriteLine(String.Format("batch {0} starting...", i));
-                SaveSupportersToSalsa(salsa, i, blockSize);
+                SaveSupportersToSalsa(db, salsa, i, blockSize);
             }
 
-            db.Dispose();
-            DateTime finished = DateTime.Now;
+            var finished = DateTime.Now;
             Console.WriteLine("finished:" + (finished - begin).TotalSeconds);
         }
     }
