dc6e83396f29a365fadf9280036b67728b0f7bde
pzhao <peter.qs.zhao@gmail.com>
2012-07-30 14:29:18 -0400

    add missing custom exception files

---
+++ SalsaImporter/Exceptions/CreateSupportersException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SalsaImporter.Exceptions
+{
+    public class CreateSupportersException: ApplicationException
+    {
+        public CreateSupportersException(string message, Exception internalException)
+            :base(message, internalException){}
+    }
+}
---
+++ SalsaImporter/Exceptions/InvalidSalsaResponseException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalsaImporter.Exceptions
+{
+    public class InvalidSalsaResponseException : ApplicationException
+    {
+        public InvalidSalsaResponseException(string message, Exception internalException)
+            : base(message, internalException)
+        {
+        }
+    }
+}
