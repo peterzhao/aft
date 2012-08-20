416af8530c383b611a3bdca48a162a40a3e89532
Tom Alexandrowicz <talexand@thoughtworks.com>
2012-08-20 18:19:46 -0400

    First pass at a SupporterGroups object

---
+++ SalsaImporter/Aft/SupporterGroup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using SalsaImporter.Synchronization;
+
+namespace SalsaImporter.Aft
+{
+    public class SupporterGroup : ISyncObject
+    {
+        public int Id { get; set; }
+        public int? ExternalId { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime? ModifiedDate { get; set; }
+
+        public int SupporterId { get; set; }
+        public int GroupId { get; set; }
+
+        protected bool Equals(SupporterGroup other)
+        {
+            return GroupId == other.GroupId && SupporterId == other.SupporterId;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((SupporterGroup) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (GroupId*397) ^ SupporterId;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Id: {0}, ExternalId: {1}, ModifiedDate: {2}, SupporterId: {3}, GroupId: {4}", Id, ExternalId, ModifiedDate, SupporterId, GroupId);
+        }
+    }
+}
---
+++ SalsaImporterTests/Aft/SupporterGroupTests.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using NUnit.Framework;
+using SalsaImporter.Aft;
+
+namespace SalsaImporterTests.Aft
+{
+    [TestFixture]
+    public class SupporterGroupTests
+    {
+        [Test]
+        public void ShouldCompareOnFields()
+        {
+            var supporterGroup = new SupporterGroup { SupporterId = 123, GroupId = 456 };
+            
+            // Identity
+            Assert.AreEqual(supporterGroup, supporterGroup);
+
+            Assert.AreEqual(supporterGroup, new SupporterGroup { SupporterId = 123, GroupId = 456 });
+            Assert.AreNotEqual(supporterGroup, new SupporterGroup { SupporterId = 223, GroupId = 456 });
+            Assert.AreNotEqual(supporterGroup, new SupporterGroup { SupporterId = 123, GroupId = 256 });
+        }
+
+        [Test]
+        public void ShouldNotCompareOnIdExternalOrModifiedDate()
+        {
+            var group = new SupporterGroup { SupporterId = 123, GroupId = 456, Id = 1, ExternalId = 1, ModifiedDate = new DateTime(2012, 1, 1) };
+
+            Assert.AreEqual(group, new SupporterGroup { SupporterId = 123, GroupId = 456, Id = 2, ExternalId = 1, ModifiedDate = new DateTime(2012, 1, 1) });
+            Assert.AreEqual(group, new SupporterGroup { SupporterId = 123, GroupId = 456, Id = 1, ExternalId = 2, ModifiedDate = new DateTime(2012, 1, 1) });
+            Assert.AreEqual(group, new SupporterGroup { SupporterId = 123, GroupId = 456, Id = 1, ExternalId = 1, ModifiedDate = new DateTime(2012, 2, 1) });
+        }   
+    }
+}
