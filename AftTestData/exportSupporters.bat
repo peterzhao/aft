cd152286bb8fa8b4096cf203c6aa04e25be3501b
Tom Alexandrowicz <talexand@thoughtworks.com>
2012-08-29 13:37:59 -0400

    clean up db scripts; add field_mapping table

--- AftTestData/exportSupporters.bat
+++
@@ -1,2 +0,0 @@
-bcp Aft.dbo.Supporters format %1Supporters.dat -n -f %1Supporters.fmt -S localhost -T -a 65535
-bcp Aft.dbo.Supporters    out %1Supporters.dat -f %1Supporters.fmt -S localhost -T -a 65535
--- AftTestData/importSupporters.bat
+++
@@ -1,2 +0,0 @@
-sqlcmd -Q "TRUNCATE TABLE Aft.dbo.Supporters"
-bcp Aft.dbo.Supporters in %1Supporters.dat -q -f %1Supporters.fmt -S localhost -T -a 65535
